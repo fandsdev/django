5e6804df78e8d553b303b338acc16d9245b75c76
Fedor Borshev <fedor@borshev.com>
2020-03-08 21:26:12 +0300

    Hi there :neckbeard:

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+echo -ne Running with
+
+python --version
+
+echo Creating and populating virtualenv..
+
+python -m venv venv
+. venv/bin/activate
+
+pip install --upgrade pip
+pip install pip-tools
+pip-sync requirements.txt dev-requirements.txt
+
+
+echo Done
---
+++ {{cookiecutter.project_slug}}/update-deps.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+
+echo Running pip-compile...
+
+for dep in 'requirements.in' 'dev-requirements.in';
+do
+
+    pip-compile -q --generate-hashes $dep
+done
+
+echo Done
+
+echo Installing local deps
+
+pip-sync requirements.txt dev-requirements.txt
