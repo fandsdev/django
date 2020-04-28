bf058c2895623fc7f7d6492899113554d72b6e42
Fedor Borshev <fedor@borshev.com>
2020-04-28 18:54:10 +0300

    Enabled django_filters as the default filter backend for DRF

--- {{cookiecutter.project_slug}}/src/app/settings.py
+++ {{cookiecutter.project_slug}}/src/app/settings.py
@@ -108,6 +108,9 @@ USE_TZ = True
 
 MAX_PAGE_SIZE = env('MAX_PAGE_SIZE', cast=int, default=1000)
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
