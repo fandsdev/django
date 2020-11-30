e562deeb74e5282dd9de64f2742625e503fed2a9
Fedor Borshev <fedor@borshev.com>
2020-11-30 18:56:55 +0300

    Better BaseConfig docs

--- {{cookiecutter.project_slug}}/src/app/config.py
+++ {{cookiecutter.project_slug}}/src/app/config.py
@@ -7,7 +7,9 @@ class AppConfig(BaseAppConfig):
     """Default App configuration template. Import app.signals.handers.
 
     We do not recomend you to use django signals at all.
-    Check out https://lincolnloop.com/blog/django-anti-patterns-signals/ if you know nothing about it.
+    Check out https://lincolnloop.com/blog/django-anti-patterns-signals/ if
+    you know nothing about that.
+
     Allthough, if you wish to use signals, place handlers to the `signals/handlers.py`:
     your code be automatically imported and used.
     """
