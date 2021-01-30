da87891f8c43bba2ddf72370a2406a7fe75bed60
Fedor Borshev <fedor@borshev.com>
2021-01-30 16:24:54 +0300

    Added django-ipware

---
+++ {{cookiecutter.project_slug}}/src/app/tests/tests_remote_addr_midlleware.py
@@ -0,0 +1,28 @@
+import pytest
+
+from django.apps import apps
+
+from app.test.api_client import DRFClient
+
+pytestmark = [pytest.mark.django_db]
+
+
+@pytest.fixture(autouse=True)
+def _require_users_app_installed(settings):
+    assert apps.is_installed('users'), """
+        Stock f213/django users app should be installed to run this test.
+
+        Make sure to test app.middleware.real_ip.real_ip_middleware on your own, if you drop
+        the stock users app.
+    """
+
+
+@pytest.fixture
+def api():
+    return DRFClient(HTTP_X_FORWARDED_FOR='100.200.250.150, 10.0.0.1')
+
+
+def test(api):
+    got = api.get('/api/v1/users/me/')
+
+    assert got['remote_addr'] == '100.200.250.150'
