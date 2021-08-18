03929a5b53e3704111fc7fc494c57853fbd956aa
Fedor Borshev <fedor@borshev.com>
2021-08-18 16:33:23 +0300

    closes #277 -- migrated to bcrypt

--- {{cookiecutter.project_slug}}/src/app/conf/auth.py
+++ {{cookiecutter.project_slug}}/src/app/conf/auth.py
@@ -10,3 +10,20 @@ JWT_AUTH = {
     'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=21),
     'JWT_ALLOW_REFRESH': True,
 }
+
+
+#
+# Security notice: we use plain bcrypt to store passwords.
+#
+# We avoid django default pre-hashing algorithm
+# from contrib.auth.hashers.BCryptSHA256PasswordHasher.
+#
+# The reason is compatibility with other hashing libraries, like
+# Ruby Devise or Laravel default hashing algorithm.
+#
+# This means we can't store passwords longer then 72 symbols.
+#
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+]
---
+++ {{cookiecutter.project_slug}}/src/users/tests/test_password_hashing.py
@@ -0,0 +1,14 @@
+import pytest
+import uuid
+
+from users.models import User
+
+pytestmark = [pytest.mark.django_db]
+
+
+def test():
+    user = User.objects.create(username=str(uuid.uuid4()))
+    user.set_password('l0ve')
+    user.save()
+
+    assert user.password.startswith('bcrypt')
