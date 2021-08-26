9fec0c7658a1a2ee328149825b020746107d9466
Denis Surkov <surkov0denis@yandex.ru>
2021-08-26 22:55:38 +0500

    Add CycleFixtureFactory

--- {{cookiecutter.project_slug}}/src/sepulkas/tests/api/test_list_sepulkas.py
+++ {{cookiecutter.project_slug}}/src/sepulkas/tests/api/test_list_sepulkas.py
@@ -8,3 +8,11 @@ def test_list_sepulkas(as_anon, sepulka):
 
     assert got['results'][0]['title'] == 'The Sepulka'
     assert got['results'][0]['coverImage'].endswith('.gif')
+
+
+def test_sepulkas_count(as_anon, factory):
+    factory.cycle(10).sepulka()
+
+    got = as_anon.get('/api/v1/sepulkas/')
+
+    assert got['count'] == 10
