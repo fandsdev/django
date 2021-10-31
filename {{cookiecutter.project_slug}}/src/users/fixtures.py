8afbef872c102f5f83375ced5754c88438d77ebd
Slava Nabatchikov <nvo87@yandex.ru>
2021-10-31 13:56:50 +0300

    remove unused fixtures

--- {{cookiecutter.project_slug}}/src/users/fixtures.py
+++ {{cookiecutter.project_slug}}/src/users/fixtures.py
@@ -2,15 +2,5 @@ import pytest
 
 
 @pytest.fixture
-def anon(factory):
-    return factory.anon()
-
-
-@pytest.fixture
 def user(factory):
     return factory.user()
-
-
-@pytest.fixture
-def staff(factory):
-    return factory.user(is_staff=True)
