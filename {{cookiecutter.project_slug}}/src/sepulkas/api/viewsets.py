f43ef33d703edb6a2d3081ab80b7a8470c8a41a9
Fedor Borshev <fedor@borshev.com>
2022-02-23 18:52:23 +0300

    viewset fix

--- {{cookiecutter.project_slug}}/src/sepulkas/api/viewsets.py
+++ {{cookiecutter.project_slug}}/src/sepulkas/api/viewsets.py
@@ -1,10 +1,10 @@
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ModelViewSet
 
 from sepulkas.api import serializers
 from sepulkas.models import Sepulka
 
 
-class SepulkaViewSet(ViewSet):
+class SepulkaViewSet(ModelViewSet):
     serializer_class = serializers.SepulkaSerializer
     serializer_action_classes = {
         'create': serializers.SepulkaCreateSerializer,
