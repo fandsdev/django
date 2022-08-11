4e4685e2b6b755f4dc4a8d0e22d7d78c6c90bcc9
denisSurkov <surkov0denis@yandex.ru>
2022-08-11 12:08:32 +0500

    Add parametrize

--- {{cookiecutter.project_slug}}/src/app/tests/test_on_transaction_commit.py
+++ {{cookiecutter.project_slug}}/src/app/tests/test_on_transaction_commit.py
@@ -16,33 +16,16 @@ def dumb_function(mocker: MockFixture):
     return mocker.MagicMock()
 
 
-@pytest.fixture
-def _on_commit_always_eager_false(settings):
-    settings.ON_COMMIT_ALWAYS_EAGER = False
-
-
-@pytest.fixture
-def _on_commit_always_eager_true(settings):
-    settings.ON_COMMIT_ALWAYS_EAGER = True
-
-
-@pytest.mark.usefixtures('_on_commit_always_eager_false')   # noqa: AAA01
-def test_doesnt_call_before_commit(dumb_function, django_capture_on_commit_callbacks):
+@pytest.mark.parametrize('setting_value, called_times_in_transaction, called_times_after_transaction',
+                         [
+                             (True, 1, 1),
+                             (False, 0, 1),
+                         ])
+def test_doesnt_call_before_commit(dumb_function, django_capture_on_commit_callbacks, settings, setting_value, called_times_in_transaction, called_times_after_transaction):
     wrapped = on_transaction_commit(dumb_function)  # noqa: AAA01
 
     with django_capture_on_commit_callbacks(execute=True), atomic():
         wrapped()
-        dumb_function.assert_not_called()
+        assert dumb_function.call_count == called_times_in_transaction
 
-    dumb_function.assert_called_once()
-
-
-@pytest.mark.usefixtures('_on_commit_always_eager_true')  # noqa: AAA01
-def test_calls_if_flag_enabled(dumb_function, django_capture_on_commit_callbacks):
-    wrapped = on_transaction_commit(dumb_function)   # noqa: AAA01
-
-    with django_capture_on_commit_callbacks(execute=True), atomic():
-        wrapped()
-        dumb_function.assert_called_once()
-
-    dumb_function.assert_called_once()
+    assert dumb_function.call_count == called_times_after_transaction
