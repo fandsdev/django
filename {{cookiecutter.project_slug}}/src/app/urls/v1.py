98e1033dad881de94004fbca5ebbfde421ecfff4
Slava Nabatchikov <nvo87@fands.dev>
2023-11-04 16:53:12 +0300

    bumped python and django. Updated .django-app-template.

--- {{cookiecutter.project_slug}}/src/app/urls/v1.py
+++ {{cookiecutter.project_slug}}/src/app/urls/v1.py
@@ -6,8 +6,8 @@ from django.urls import path
 
 app_name = "api_v1"
 urlpatterns = [
-    path("auth/", include("a12n.urls")),
-    path("users/", include("users.urls")),
+    path("auth/", include("a12n.api.urls")),
+    path("users/", include("users.api.urls")),
     path("healthchecks/", include("django_healthchecks.urls")),
     path("docs/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("docs/swagger/", SpectacularSwaggerView.as_view(url_name="schema")),
