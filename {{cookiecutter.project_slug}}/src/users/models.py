51351ef010527dc7885a77c4c64df00f552b0b79
Slava Nabatchikov <nvo87@fands.dev>
2023-11-21 13:52:30 +0300

    updated requirements txt

--- {{cookiecutter.project_slug}}/src/users/models.py
+++ {{cookiecutter.project_slug}}/src/users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import UserManager as _UserManager
+from typing import ClassVar
 
 
-class User(AbstractUser):
-    objects = _UserManager()  # type: _UserManager
+class User(AbstractUser):  # noqa
+    objects: ClassVar[_UserManager] = _UserManager()
