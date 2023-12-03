d66af0e4c213fe748faa20933e06e469f4f9867b
hnthh <anyaagarenko@gmail.com>
2023-12-04 02:31:51 +0300

    remove migrations file from app template

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -5,7 +5,8 @@ cp src/core/.env.ci src/core/.env
 poetry install
 
 poetry run python src/manage.py collectstatic
-poetry run python src/manage.py migrate
 poetry run python src/manage.py startapp some_app --entity_name some_entity
+poetry run python src/manage.py makemigrations --name some_entity
+poetry run python src/manage.py migrate
 
 make checks test
