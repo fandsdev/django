265240750a3790df589e4413ab39b14a1e51ed0d
hnthh <anyaagarenko@gmail.com>
2024-01-11 17:24:02 +0300

    remove old users migration, create new one in post gen hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -9,6 +9,9 @@ poetry install
 
 poetry run python src/manage.py collectstatic
 poetry run python src/manage.py startapp some_app
+poetry run python src/manage.py makemigrations -n "initial"
 poetry run python src/manage.py migrate
+
+poetry run isort src/users/migrations/0001_initial.py
 
 make lint test
