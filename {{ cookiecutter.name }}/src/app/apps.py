cc99f1557004977777c83a92c2a3443d77f32928
Anya Agarenko <anyaagarenko@gmail.com>
2024-05-27 12:57:04 +0300

    fix startapp management cmd (#655)

---
+++ {{ cookiecutter.name }}/src/app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig as DjangoAppConfig
+
+
+class AppConfig(DjangoAppConfig):
+    name = "app"
--- {{ cookiecutter.name }}/src/app/management/commands/startapp.py
+++ {{ cookiecutter.name }}/src/app/management/commands/startapp.py
@@ -1,5 +1,3 @@
-from os import path
-
 from django.conf import settings
 from django.core.management.commands.startapp import Command as BaseCommand
 
@@ -8,7 +6,18 @@ class Command(BaseCommand):
     """Set custom template for all newly generated apps"""
 
     def handle(self, **options):
+        if "directory" not in options or options["directory"] is None:
+            directory = settings.BASE_DIR.parent / options["name"]
+
+            directory.mkdir(exist_ok=True)
+
+            options["directory"] = str(directory)
+
         if "template" not in options or options["template"] is None:
-            options["template"] = path.join(settings.BASE_DIR, ".django-app-template")
+            template = settings.BASE_DIR.parent / ".django-app-template"
+
+            options["template"] = str(template)
 
         super().handle(**options)
+
+    def validate_name(self, *args, **kwargs): ...
