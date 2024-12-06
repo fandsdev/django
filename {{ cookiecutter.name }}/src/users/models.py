09578911f9e3f1535e6daeebf55aac25b89d140b
Daniil Maltsev <da-maltsev@inbox.ru>
2024-12-06 13:03:18 +0500

    combine imports

--- {{ cookiecutter.name }}/src/users/models.py
+++ {{ cookiecutter.name }}/src/users/models.py
@@ -1,7 +1,6 @@
 from typing import ClassVar
 
-from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import UserManager as _UserManager
+from django.contrib.auth.models import AbstractUser, UserManager as _UserManager
 
 
 class User(AbstractUser):  # noqa
