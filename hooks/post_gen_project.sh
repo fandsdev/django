98a0d966d2230c5f8116c90396ae605331102d1c
Nikolay Kiryanov <nikolay@kiryanov.ru>
2025-04-22 12:53:58 +0300

    Draft: install deps for templated project with uv

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,12 +2,11 @@
 
 cp src/app/.env.ci src/app/.env
 
-poetry lock --no-update
-poetry install
+uv sync
 
-poetry run python src/manage.py collectstatic
-poetry run python src/manage.py makemigrations -n "initial"
-poetry run python src/manage.py migrate
+uv run python src/manage.py collectstatic
+uv run python src/manage.py makemigrations --name "initial"
+uv run python src/manage.py migrate
 
 make lint
 make test
