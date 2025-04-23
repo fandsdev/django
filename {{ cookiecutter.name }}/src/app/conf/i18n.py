3484ce617ef4daa62e4b13ceb744702da0b97b18
Nikolay Kiryanov <nikolay@kiryanov.ru>
2025-04-23 20:22:12 +0300

    Rename directory for locales `locale` --> `.locale`

--- {{ cookiecutter.name }}/src/app/conf/i18n.py
+++ {{ cookiecutter.name }}/src/app/conf/i18n.py
@@ -5,6 +5,6 @@ from app.conf.boilerplate import BASE_DIR
 
 LANGUAGE_CODE = "ru"
 
-LOCALE_PATHS = [Path(BASE_DIR).parent / "locale"]
+LOCALE_PATHS = [Path(BASE_DIR).parent / ".locale"]
 
 USE_i18N = True
