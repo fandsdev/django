84d3686459dbb320e3d51e18728cb825b7f4a937
Nikolay Kiryanov <nkiryanov@fands.dev>
2025-04-28 23:34:15 +0300

    Bump python 3.12, django 5.2 and related deps (#805)

--- {{ cookiecutter.name }}/src/users/api/viewsets.py
+++ {{ cookiecutter.name }}/src/users/api/viewsets.py
@@ -1,9 +1,9 @@
 from django.db.models import QuerySet
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.request import Request
 from rest_framework.response import Response
 
+from app.api.request import AuthenticatedRequest
 from users.api.serializers import UserSerializer
 from users.models import User
 
@@ -12,7 +12,9 @@ class SelfView(GenericAPIView):
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
 
-    def get(self, request: Request) -> Response:
+    request: AuthenticatedRequest
+
+    def get(self, request: AuthenticatedRequest) -> Response:
         user = self.get_object()
         serializer = self.get_serializer(user)
 
