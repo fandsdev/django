39e0c1f8d9276b0e26860f81fc864013eab5f869
Fedor Borshev <1592663+f213@users.noreply.github.com>
2025-09-09 09:43:49 +0300

    Celery tasks now fail in eager mode (#824)

--- {{ cookiecutter.name }}/src/app/conf/celery.py
+++ {{ cookiecutter.name }}/src/app/conf/celery.py
@@ -4,6 +4,7 @@ from app.conf.timezone import TIME_ZONE
 
 CELERY_BROKER_URL = env("CELERY_BROKER_URL", cast=str, default="redis://localhost:6379/0")
 CELERY_TASK_ALWAYS_EAGER = env("CELERY_TASK_ALWAYS_EAGER", cast=bool, default=env("DEBUG"))
+CELERY_TASK_EAGER_PROPAGATES = True
 CELERY_TIMEZONE = TIME_ZONE
 CELERY_ENABLE_UTC = False
 CELERY_TASK_ACKS_LATE = True
