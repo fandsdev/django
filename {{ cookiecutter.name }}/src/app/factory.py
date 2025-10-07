e613ae84930dc8ea64ff49b252a5d44c626deffc
Nikolay Kiryanov <nkiryanov@fands.dev>
2025-10-07 09:00:05 +0300

    Deps: replace mixer with faker + model_bakery (#827)

--- {{ cookiecutter.name }}/src/app/factory.py
+++ {{ cookiecutter.name }}/src/app/factory.py
@@ -1,13 +1,9 @@
 from django.core.files.uploadedfile import SimpleUploadedFile
-from faker import Faker
 
 from app.testing import register
 from app.testing.types import FactoryProtocol
 
 
-faker = Faker()
-
-
 @register
 def image(self: FactoryProtocol, name: str = "image.gif", content_type: str = "image/gif") -> SimpleUploadedFile:
-    return SimpleUploadedFile(name=name, content=faker.image(), content_type=content_type)
+    return SimpleUploadedFile(name=name, content=self.faker.image(), content_type=content_type)
