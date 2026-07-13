48338cd9c1ffc4f088b126d6450445e6987134dc
Slava Nv <nvo87@fands.dev>
2026-07-13 12:30:47 +0300

    Remove DRF session authentication for debug mode (#845)

--- {{ cookiecutter.name }}/src/app/conf/api.py
+++ {{ cookiecutter.name }}/src/app/conf/api.py
@@ -32,11 +32,6 @@ REST_FRAMEWORK = {
     "EXCEPTION_HANDLER": "app.exceptions.app_service_exception_handler",
 }
 
-# Adding session auth and browsable API at the developer machine
-if env("DEBUG", cast=bool, default=False):
-    REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"].append("rest_framework.authentication.SessionAuthentication")
-    REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"].append("djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer")
-
 
 # Set up drf_spectacular, https://drf-spectacular.readthedocs.io/en/latest/settings.html
 SPECTACULAR_SETTINGS = {
