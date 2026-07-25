242224c80dd5a1cf6ae5b52c36dd919f4788072d
Nikolay Kiryanov <nikolay@kiryanov.ru>
2026-07-25 17:41:45 +0300

    Replace django-healthchecks with our own healthcheck

--- {{ cookiecutter.name }}/src/app/tests/test_health.py
+++ {{ cookiecutter.name }}/src/app/tests/test_health.py
@@ -1,13 +1,21 @@
 import pytest
+from django.db import OperationalError
 
 
 pytestmark = [
     pytest.mark.django_db,
-    pytest.mark.filterwarnings("ignore:.*inspect.getargspec().*:DeprecationWarning"),
 ]
 
 
 def test(as_anon):
-    result = as_anon.get("/api/v1/healthchecks/db/")
+    result = as_anon.get("/api/v1/healthchecks/")
 
-    assert result == "true"
+    assert result == {"db": True}
+
+
+def test_503_when_database_is_down(as_anon, mocker):
+    mocker.patch("django.db.connection.cursor", side_effect=OperationalError)  # connection.close() is a no-op on in-memory SQLite
+
+    result = as_anon.get("/api/v1/healthchecks/", expected_status=503)
+
+    assert result == {"db": False}
